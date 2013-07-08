769447dd288c9c8d301daf32d5ce247b08809ff5
nadrimajstor <ipejic@gmail.com>
2013-07-08 23:23:47 +0200

    web.js - Fixed missing: require('fs')

--- web.js
+++ web.js
@@ -2,12 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var fs = new fs.readFileSync("index.html");
+var fs = require('fs');
+var fs_buffer_index = fs.readFileSync("index.html");
 // fs.readFileSync(filename, [options])
 // Synchronous version of fs.readFile. Returns the contents of the filename.
 // If the encoding option is specified then this function returns a string. Otherwise it returns a buffer.
 
-var index = fs.toString();
+var index = fs_buffer_index.toString();
 
 app.get('/', function(request, response) {
   response.send(index);
