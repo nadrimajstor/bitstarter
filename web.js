6ed3de15e4640bd9f42a1a5db85ba5ec0ae7b6ec
nadrimajstor <ipejic@gmail.com>
2013-07-08 23:09:45 +0200

    Modified web.js - serve / from index.html

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = new fs.readFileSync("index.html");
+// fs.readFileSync(filename, [options])
+// Synchronous version of fs.readFile. Returns the contents of the filename.
+// If the encoding option is specified then this function returns a string. Otherwise it returns a buffer.
+
+var index = fs.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
